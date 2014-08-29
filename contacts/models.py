4b889c28728563e2e8a93f5b36ade9b5c0b68509
mightyfrancis <lovencorpse@gmail.com>
2014-08-29 16:46:02 +0800

    added Address model
    can view address details. empty fields are unhandled

--- contacts/models.py
+++ contacts/models.py
@@ -5,11 +5,24 @@ class Contact(models.Model):
 
     first_name = models.CharField(max_length=255,)
     last_name = models.CharField(max_length=255,)
+    middle_name = models.CharField(max_length=255,)
 
     email = models.EmailField()
 
     def __str__(self):
-        return ' '.join([self.first_name,self.last_name,])
+        return ' '.join([self.first_name,self.last_name,self.middle_name,])
 
     def get_absolute_url(self):
         return reverse('contacts-view', kwargs={'pk': self.id})
+
+class Address(models.Model):
+
+    contact = models.ForeignKey(Contact)
+    address_type = models.CharField(max_length=10,)
+    address = models.CharField(max_length=255,)
+    city = models.CharField(max_length=255,)
+    state = models.CharField(max_length=2,)
+    postal_code = models.CharField(max_length=20,)
+
+    class Meta:
+        unique_together = ('contact', 'address_type',)
